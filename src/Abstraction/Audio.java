8680e23dbf3a0b64acb1725238cd17583fdd5ba0
janedubrov <stupakova2012@gmail.com>
2026-04-14 13:44:05 +0200

    Added example Abtraction

---
+++ src/Abstraction/Audio.java
@@ -0,0 +1,15 @@
+package Abstraction;
+
+public class Audio extends MediaFile{
+    String artist;
+
+    public Audio(String title, double size, String artist) {
+        super(title, size);
+        this.artist = artist;
+    }
+
+    @Override
+    void play() {
+        System.out.println("Playing song " + title + " by artist: " + artist);
+    }
+}
