f8e54907dea360a1904bc9a988d846e18e87b077
janedubrov <stupakova2012@gmail.com>
2026-06-19 11:15:18 +0200

    Custom Exception + Array

---
+++ src/exceptions/Atm.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+public class Atm {
+    public static void main(String[] args) {
+
+        int balance = 1000;
+        int[] amounts = {150, 250, 1500, 1000};
+
+        for(int a : amounts) {
+            try{
+                withdraw(balance, a);
+
+            } catch(InsufficientFundsException e ) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    static void withdraw(int balance, int amount) throws InsufficientFundsException{
+        if(amount > balance) {
+            throw new InsufficientFundsException("You can't withdraw "  + amount + " Euro - ! Insufficient funds.");
+        } else {
+            int newBalance = balance - amount;
+            System.out.println("You withdrew " + amount + " Your new balance is: " + newBalance + " Euro");
+        }
+    }
+}
